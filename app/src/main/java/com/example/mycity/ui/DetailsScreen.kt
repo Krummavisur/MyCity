39d838c829b684e30ec28e89efaf139b4bad50ce
krummavisur <dmitri4enko0701@gmail.com>
2025-03-26 00:50:04 +0500

    SixthCom

--- app/src/main/java/com/example/mycity/ui/DetailsScreen.kt
+++ app/src/main/java/com/example/mycity/ui/DetailsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -24,14 +26,21 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mycity.model.PlaceInfo
+import com.example.mycity.ui.details.DetailsViewModel
 import com.example.mycity.ui.theme.MyCityTheme
 
 @Composable
-fun DetailsScreen() {
-    Box(modifier = Modifier.fillMaxSize()){
+fun DetailsScreen(
+    viewModel: DetailsViewModel
+) {
+    val place = viewModel.uiState.collectAsState().value
+
+    Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn {
             item {
-                InformationList()
+                place?.let {
+                    InformationList(place = it)
+                }
             }
         }
     }
@@ -39,7 +48,7 @@ fun DetailsScreen() {
 
 @Composable
 private fun InformationList(
-    place: PlaceInfo,
+    place: PlaceInfo
 ) {
     Box(
         modifier = Modifier
@@ -48,8 +57,8 @@ private fun InformationList(
             .background(
                 Brush.verticalGradient(
                     colors = listOf(Color(0xFFE44D26), Color.Black)
+                )
             )
-        )
     ) {
         Image(
             painter = painterResource(id = place.image),
@@ -61,7 +70,7 @@ private fun InformationList(
                 .padding(end = 16.dp)
         )
 
-        Column (
+        Column(
             modifier = Modifier
                 .align(Alignment.BottomStart)
                 .padding(16.dp)
@@ -72,6 +81,8 @@ private fun InformationList(
                 fontWeight = FontWeight.Bold,
                 color = Color.White
             )
+            Spacer(modifier = Modifier.height(8.dp))
+
             Text(
                 text = stringResource(id = place.details),
                 fontSize = 16.sp,
--- app/src/main/java/com/example/mycity/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/example/mycity/ui/details/DetailsViewModel.kt
@@ -1,12 +1,30 @@
 package com.example.mycity.ui.details
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.example.mycity.data.DataSource
+import com.example.mycity.model.PlaceInfo
+import com.example.mycity.ui.places.PlacesViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
-class DetailsViewModel: ViewModel() {
-    private val _uiState = MutableStateFlow(
+class DetailsViewModel(
+    private val placesViewModel: PlacesViewModel,
+    private val placeIndex: Int
+        ): ViewModel() {
+    private val _uiState = MutableStateFlow<PlaceInfo?>(null)
 
-    )
-    val uiState: StateFlow<>
+    val uiState: StateFlow<PlaceInfo?> = _uiState
+
+    init {
+        loadPLaceDetails()
+    }
+
+    private fun loadPLaceDetails() {
+        val place = placesViewModel.uiState.value.placesList.getOrNull(placeIndex)
+
+            _uiState.update { place }
+    }
 }
+
